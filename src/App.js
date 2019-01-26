490fba2ee6bef12a041e9779e954bf0a3e4d9962
Mathew Varughese <mat.varughese3@gmail.com>
2019-01-26 15:19:46 -0500

    create query for pi data

--- src/App.js
+++ src/App.js
@@ -30,13 +30,24 @@ class App extends Component {
     });
   };
 
+  getPiDataQuery = (piid, options) => {
+    const NOW_SECONDS = Date.now()/1000;
+    const HOUR_SECONDS = 60 * 60;
+    const DAY_SECONDS =   HOUR_SECONDS* 24;
+    const WEEK_SECONDS = DAY_SECONDS * 7;
+    const MONTH_SECONDS = WEEK_SECONDS * 31;
+    const lastTime = NOW_SECONDS - HOUR_SECONDS; 
+    return this.db.ref(`/data/${piid}`).orderByKey().endAt(lastTime);
+  }
+
   render() {
     return (
       <Router>
         <Container fluid style={{padding: 0}}>
           <Header />
           <Switch>
-            <Route exact path="/" render={props => <Home {...props} data={this.state.data} />} />
+            <Route exact path="/" 
+              render={props => <Home {...props} data={this.state.data} getPiDataQuery={this.getPiDataQuery} />} />
             <Route exact path='/building/:id'
               render={props => <BuildingDetail {...props} data={this.state.data} />}
             />
