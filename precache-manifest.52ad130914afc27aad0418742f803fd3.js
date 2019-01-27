2e1f3358c835acbe3604e688724d5c1c87153b7a
Andrew Henry <imandrewh@outlook.com>
2019-01-27 00:31:12 -0500

    Updates

---
+++ precache-manifest.52ad130914afc27aad0418742f803fd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "793ddc7269d44b90c3f6",
+    "url": "/counting-app/static/js/runtime~main.793ddc72.js"
+  },
+  {
+    "revision": "9871132806eea431ddaa",
+    "url": "/counting-app/static/js/main.98711328.chunk.js"
+  },
+  {
+    "revision": "5ba2b5ad22acc7f9efff",
+    "url": "/counting-app/static/js/1.5ba2b5ad.chunk.js"
+  },
+  {
+    "revision": "9871132806eea431ddaa",
+    "url": "/counting-app/static/css/main.f8002aa6.chunk.css"
+  },
+  {
+    "revision": "e8574a98149aeb88702a5b633f9600c8",
+    "url": "/counting-app/index.html"
+  }
+];
